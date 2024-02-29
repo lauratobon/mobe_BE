8a491808ae72930e5d70459502c483173a124676
lauraT <lauratobon@gmail.com>
2024-02-29 17:35:50 -0500

    first commit

---
+++ src/main/java/mobe/mobe_app/MobeAppApplication.java
@@ -0,0 +1,13 @@
+package mobe.mobe_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobeAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mobe/mobe_app/MobeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package mobe.mobe_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
